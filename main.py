8ef962e9ddf9d4dc10aaacaca5986afae699d4e8
Arnab <74238471+breeze37@users.noreply.github.com>
2023-05-16 23:05:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ imgElon = face_recognition.load_image_file('SampleImages/elon.jpg')
 imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('SampleImages/elon tilt.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
-
+##img
 faceLoc = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 cv2.rectangle(imgElon,(faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]),(255,0,0),2)
